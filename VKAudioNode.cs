078d47151bb320f2cc0e4633e8d1e5fa0ddf8b45
MaximBagashev <ThreeDISevenZeroR@gmail.com>
2016-03-08 01:06:28 +0400

    Добавление новых нод, в связи с скрытием захардкоденых ключей под функционалом констант.

---
+++ VKAudioNode.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+using ThreeDISevenZeroR.SpeechSequencer.Core;
+using System.ComponentModel;
+using System.Net;
+using System.IO;
+using System.Xml;
+
+namespace ThreeDISevenZeroR.SpeechSequencer.Extra
+{
+    [XmlElementBinding("VKAudio")]
+    [Description("Проигрывает музыку из соцсети вконтакте")]
+    public class VKAudioNode : ValuePlaybackNode
+    {
+        private const string c_apiSearch = "https://api.vk.com/method/audio.search.xml?access_token={0}&q={1}&count=1";
+
+        [ConstantBinding("VKApiKey")]
+        [XmlAttributeBinding]
+        [Description("Ключ доступа для API ВКонтакте")]
+        public string ApiKey { get; set; }
+
+        public ISampleProvider LoadAudio(string url)
+        {
+            WebRequest request = WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+
+            MemoryStream memoryStream = new MemoryStream();
+            response.GetResponseStream().CopyTo(memoryStream);
+
+            memoryStream.Position = 0;
+            return new Mp3FileReader(memoryStream).ToSampleProvider();
+        }
+
+        protected override ISampleProvider CreateProvider(string value, IPlaybackContext context)
+        {
+            WebRequest request = WebRequest.Create(string.Format(c_apiSearch, ApiKey, value));
+            WebResponse response = request.GetResponse();
+            XmlDocument document = new XmlDocument();
+            document.Load(response.GetResponseStream());
+            XmlElement element = (XmlElement)document.SelectSingleNode(".//audio/url");
+
+            if (element != null)
+            {
+                return LoadAudio(element.InnerText);
+            }
+            else
+            {
+                IAudioNode audio = ("Не удалось найти аудио по запросу " + value).WrapStringAsSpeech();
+                audio.InitNewState(context);
+                return audio;
+            }
+        }
+    }
+}
---
+++ YandexTranslateNode.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using ThreeDISevenZeroR.SpeechSequencer.Core;
+
+namespace ThreeDISevenZeroR.SpeechSequencer.Extra
+{
+    [XmlElementBinding("YandexTranslate")]
+    [Description("Переводит лежащее внутри значение через Yandex переводчик")]
+    public class YandexTranslateNode : ValueModifierNode
+    {
+        public static readonly string c_urlTemplate = "https://translate.yandex.net/api/v1.5/tr/translate?key={0}&text={1}&lang={2}";
+
+        protected string m_translatedContent;
+
+        [XmlAttributeBinding]
+        [Description("Язык перевода")]
+        public string Language { get; set; } = "ru-en";
+
+        [ConstantBinding("YandexApiKey")]
+        [XmlAttributeBinding]
+        [Description("Ключ для доступа к API переводчика")]
+        public string ApiKey { get; set; }
+
+        public override string ProcessValue(string value)
+        {
+            string content = Uri.EscapeDataString(value);
+
+            WebRequest request = WebRequest.Create(string.Format(c_urlTemplate, ApiKey, content, Language));
+            WebResponse response = request.GetResponse();
+
+            XmlDocument document = new XmlDocument();
+            document.Load(response.GetResponseStream());
+
+            return document.InnerText;
+        }
+    }
+}
